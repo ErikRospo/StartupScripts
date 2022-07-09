fea2d7a72fbd1b63037ad07b0ee0822c336f10af
ErikRospo <87732519+ErikRospo@users.noreply.github.com>
2022-07-09 15:02:36 -0700

    create script.sh

---
+++ script.sh
@@ -0,0 +1,5 @@
+sudo apt update;
+sudo apt upgrade;
+sudo apt install tmux htop git nodejs python3 python3-pip tree wget curl nmap man info nano build-essentials;
+sudo apt autoremove;
+sudo apt autoclean;
