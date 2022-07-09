9266340481014fd88466e6d1e1114c414d7d3097
ErikRospo <87732519+ErikRospo@users.noreply.github.com>
2022-07-09 15:02:51 -0700

    Delete script.sh

--- script.sh
+++
@@ -1,5 +0,0 @@
-sudo apt update;
-sudo apt upgrade;
-sudo apt install tmux htop git nodejs python3 python3-pip tree wget curl nmap man info nano build-essentials;
-sudo apt autoremove;
-sudo apt autoclean;
